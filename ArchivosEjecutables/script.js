1b88c87cf0d5c7648d4bf75eef410afe7c1b819a
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2024-08-08 12:01:31 -0400

    Comienzo del proyecto!

---
+++ ArchivosEjecutables/script.js
@@ -0,0 +1,66 @@
+function cambiar()
+{
+    // EN DIFERENTES IDIOMAS...
+
+    // document.getElementById("titulo").innerHTML = "TITEL VERANDER"; // EN AFRIKAANS.
+    // document.getElementById("titulo").innerHTML = "TÍTULO UKHAMARAKI"; // EN AIMARA.
+    // document.getElementById("titulo").innerHTML = "WƆASESA ATIRIM NO"; // EN AKAN.
+    // document.getElementById("titulo").innerHTML = "TITULLI I NDRYSHUAR"; // EN ALBANÉS.
+    // document.getElementById("titulo").innerHTML = "TITEL GEÄNDERT"; // EN ALEMÁN.
+    // document.getElementById("titulo").innerHTML = "ርዕስ ተለውጧል"; // EN AMÁRICO.
+    // document.getElementById("titulo").innerHTML = "تم تغيير العنوان"; // EN ÁRABE.
+    // document.getElementById("titulo").innerHTML = "ԱՆՎԱՆՈՒՄԸ ՓՈԽՎԵՑ"; // EN ARMENIO.
+    //document.getElementById("titulo").innerHTML = "TITLE সলনি হ'ল"; // EN ASAMÉS.
+    // document.getElementById("titulo").innerHTML = "BAŞLIQ DƏYİŞİLDİ"; // EN AZERBAIYANO.
+    // document.getElementById("titulo").innerHTML = "TITLE YƐLƐMA"; // EN BAMBARA.
+    // document.getElementById("titulo").innerHTML = "শিরোনাম পরিবর্তন করা হয়েছে৷"; // EN BENGALÍ.
+    // document.getElementById("titulo").innerHTML = "टाइटिल बदल गइल बा"; // EN BHOYAPURÍ.
+    // document.getElementById("titulo").innerHTML = "НАЗВА ЗМЕНЕНА"; // EN BIELORRUSO.
+    // document.getElementById("titulo").innerHTML = "ခေါင်းစဉ်ပြောင်းထားသည်။"; // EN BIRMANO.
+    // document.getElementById("titulo").innerHTML = "NASLOV PROMIJENJEN"; // EN BOSNIO.
+    // document.getElementById("titulo").innerHTML = "ЗАГЛАВИЕТО СЕ ПРОМЕНИ"; // EN BÚLGARO.
+    // document.getElementById("titulo").innerHTML = "ಶೀರ್ಷಿಕೆ ಬದಲಾಗಿದೆ"; // EN CANARÉS.
+    // document.getElementById("titulo").innerHTML = "TÍTOL CANVIAT"; // EN CATALÁN.
+    // document.getElementById("titulo").innerHTML = "NAUSAB ANG TITULO"; // EN CEBUANO.
+    // document.getElementById("titulo").innerHTML = "NÁZEV ZMĚNĚN"; // EN CHECO.
+    // document.getElementById("titulo").innerHTML = "标题已更改"; // EN CHINO (SIMPLIFICADO).
+    // document.getElementById("titulo").innerHTML = "标题已更改"; // EN CHINO (TRADICIONAL).
+    // document.getElementById("titulo").innerHTML = "මාතෘකාව වෙනස් කරන ලදී"; // EN CINGALÉS.
+    // document.getElementById("titulo").innerHTML = "제목이 변경됨" // EN COREANO.
+    // document.getElementById("titulo").innerHTML = "TITULU CAMBIAT" // EN CORSO.
+    // document.getElementById("titulo").innerHTML = "TIT CHANJE" // EN CRIOLLO HAITIANO.
+    // document.getElementById("titulo").innerHTML = "NASLOV PROMIJENJEN"; // EN CROATA.
+    // document.getElementById("titulo").innerHTML = "TITEL ÆNDRET"; // EN DANÉS.
+    // document.getElementById("titulo").innerHTML = "ސުރުޚީ ބަދަލުވެއްޖެ"; // EN DIVEHI.
+    // document.getElementById("titulo").innerHTML = "टाइटल बदल गया"; // EN DOGRI.
+    // document.getElementById("titulo").innerHTML = "NÁZOV ZMENENÝ"; // EN ESLOVACO.
+    // document.getElementById("titulo").innerHTML = "NASLOV SPREMENJEN"; // EN ESLOVENO.
+    // document.getElementById("titulo").innerHTML = "Título cambiado"; // ORIGINAL (ESPAÑOL).
+    // document.getElementById("titulo").innerHTML = "TITOLO ŜANJITA"; // EN ESPERANTO.
+    // document.getElementById("titulo").innerHTML = "PEALKIRI MUUDETUD"; // EN ESTONIO.
+    // document.getElementById("titulo").innerHTML = "IZENBURUA ALDATUA"; // EN VASCO (EUSKERA).
+    // document.getElementById("titulo").innerHTML = "WOTRƆ TANYA ƑE TANYA"; // EN EWÉ.
+    // document.getElementById("titulo").innerHTML = "OTSIKKO MUUTETTU"; // EN FINÉS.
+    // document.getElementById("titulo").innerHTML = "TITRE CHANGÉ"; // EN FRANCÉS.
+    document.getElementById("titulo").innerHTML = "TITEL feroare"; // EN FRISÓN OCCIDENTAL.
+    // document.getElementById("titulo").innerHTML = "Title changed"; // EN INGLÉS.
+    // document.getElementById("titulo").innerHTML = "Título alterado"; // EN PORTUGUÉS.
+    // document.getElementById("titulo").innerHTML = "SUIA IGOA"; // EN SAMOANO.
+    // document.getElementById("titulo").innerHTML = "НАСЛОВ ПРОМЕЊЕН"; // EN SERBIO.
+    // document.getElementById("titulo").innerHTML = "Musoro wachinjwa"; // EN SHONA.
+    // document.getElementById("titulo").innerHTML = "عنوان تبديل ڪيو ويو"; // EN SINDI.
+    // document.getElementById("titulo").innerHTML = "Ciwaanka waa la bedelay"; // EN SOMALÍ. 
+    // document.getElementById("titulo").innerHTML = "Sehlooho fetotseng"; // EN SOTHO MERIDIONAL.
+    // document.getElementById("titulo").innerHTML = "Title e fetola"; // EN SOTHO SEPTENTRIONAL.
+    // document.getElementById("titulo").innerHTML = "Kichwa imebadilishwa"; // EN SUAJILI.
+    // document.getElementById("titulo").innerHTML = "Judul robah"; // EN SUNDANÉS.
+
+
+}
+
+// MEDIANTE UN EVENTO LLAMADO "onclick"...
+
+document.getElementById("boton").onclick = function ()
+{
+    cambiar(); // LLAMADO DE LA FUNCIÓN...
+}
